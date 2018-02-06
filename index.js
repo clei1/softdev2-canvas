c42a2f397d75cb31209b574eaf9a01728512e020
clei1 <clei1@stuy.edu>
2018-02-05 19:27:15 -0500

    what is wrong w me these days

--- index.js
+++ index.js
@@ -6,26 +6,31 @@ st = true; //true is circle, false is square
 clear = document.getElementById("clear");
 toggle = document.getElementById("toggle");
 
+ctx.beginPath();
+
 var shape = function(){
+    let x = event.offsetX; let y = event.offsetY;
+    ctx.lineTo(x, y);
+    ctx.stroke();
     ctx.beginPath();
-    let x = event.offsetX;
-    let y = event.offsetY;
     if (st) {
 	ctx.arc(x, y, 10, 0, 2 * Math.PI);
+	ctx.fill();
     }
     else{
 	ctx.fillRect(x, y, 10, 10);
     }
-    ctx.fill();
-}
+    ctx.moveTo(x, y); //so that lines start at circle's center
+};
 
 var tog = function(){
     st = !st;
-}
+};
 
 var cl = function(){
     ctx.clearRect(0, 0, 500, 500);
-}
+    ctx.beginPath();
+};
 
 c.addEventListener("click", shape);
 toggle.addEventListener("click", tog);
